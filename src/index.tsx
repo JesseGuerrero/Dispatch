034e0b12bd78defaf79b9c80608d343f7d47a13b
Jesse <jesseguerrero1991@gmail.com>
2023-12-16 22:13:26 -0600

    Update repository name

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Router basename={"/Dispatch-Frontend"}>
+    <Router basename={"/Dispatch"}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
